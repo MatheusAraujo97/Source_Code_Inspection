8157f2b876577957e8b4e88a616609b8a1feb1f5
31752284 <31752284@099526-P31S202.MackSP.br>
2019-08-27 20:26:21 -0300

    ....

--- Source Code Inspection/src/br/calebe/ticketmachine/core/Troco.java
+++ Source Code Inspection/src/br/calebe/ticketmachine/core/Troco.java
@@ -24,7 +24,7 @@ class Troco {
         papeisMoeda[4] = new PapelMoeda(50, count);
         count = 0;
         while (valor % 20 != 0) {
-            count++;
+            count++; 
         }
         papeisMoeda[3] = new PapelMoeda(20, count);
         count = 0;
